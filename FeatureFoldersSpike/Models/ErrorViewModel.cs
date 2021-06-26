e7fbaee2740864dd8f38028cfdb234aff1909eac
Steven D'Anna <steven.danna@gmail.com>
2021-06-26 18:16:52 -0500

    Initial Commit

---
+++ FeatureFoldersSpike/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FeatureFoldersSpike.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
